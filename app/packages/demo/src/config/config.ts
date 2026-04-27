710ca021037c01411e5b5039e050c692c118401a
Timmy Jose <zoltan.jose@gmail.com>
2026-04-27 10:12:58 +0530

    feat: non-dev-client builds for android and ios

--- app/packages/demo/src/config/config.ts
+++ app/packages/demo/src/config/config.ts
@@ -1,3 +1,10 @@
 import { Platform } from 'react-native'
 
-export const BASE_BACKEND_URL  = Platform.OS === 'android' ? 'http://10.0.2.2:9999' : 'http://127.0.0.1:9999'
+export const IS_MAESTRO_DEV =
+  process.env.EXPO_PUBLIC_MAESTRO_DEV === 'true'
+
+export const BASE_BACKEND_URL =
+  process.env.EXPO_PUBLIC_BASE_BACKEND_URL
+  ?? (Platform.OS === 'android'
+    ? 'http://10.0.2.2:9999'
+    : 'http://127.0.0.1:9999')
